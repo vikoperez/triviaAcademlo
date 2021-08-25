15afcb38948353ddd5950a12177f80e3d6d7c038
vikoperez <k-kratoss@hotmail.com>
2021-08-25 10:28:47 -0500

    primer commit trivia

---
+++ main.js
@@ -0,0 +1,21 @@
+// https://opentdb.com/api.php?amount=8&category=24&difficulty=medium&type=multiple
+let triviaForm = document.getElementById("triviaForm");
+
+const crearApiUrl = e => {
+    e.preventDefault();
+    let amount = document.getElementById("amount").value;
+    let category = document.getElementById("category").value;
+    let difficulty = document.getElementById("difficulty").value;
+    let type = document.getElementById("type").value;
+    const API = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`
+    fetchDataApi (API) 
+}
+
+const fetchDataApi = url => {
+    fetch (url)
+    .then (response => response.json ())
+    .then (result => console.log(result.results))
+    .catch (err => console.log(err));
+};
+
+triviaForm.onsubmit = crearApiUrl;
